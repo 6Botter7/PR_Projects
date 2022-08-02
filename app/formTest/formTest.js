3855b1a45fa5c86716b48c9c1230279355445ea9
6Botter7 <7williamsandre7@gmail.com>
2022-08-02 23:20:21 +0200

    Form Created
    Basic Markup and Css done for form
    
    JS attempted

---
+++ app/formTest/formTest.js
@@ -0,0 +1,46 @@
+export default class FormTest extends crsbinding.classes.ViewBase {
+    async connectedCallback() {
+        await super.connectedCallback();
+    }
+}
+
+// let firstName = document.getElementById("firstname").value;
+// let lastName = document.getElementById("lastname").value;
+// let person = `This is ${firstName} ${lastName}`;
+// let booking = document.querySelector(".result");
+
+//     firstName.oninput = function () {
+//         booking.textContent = this.value;
+//     }
+
+//     // resultDiv= function () {
+//     //     // document.querySelector(".result").textContent = person;
+//     // }
+
+//     lastName.oninput = function () {
+//         document.getElementById("ln").textContent = this.value;
+//     }
+
+//     // var first = document.querySelector("#firstame").value = firstName
+//     // var last = document.querySelector("#lastname").value = lastName
+//     // var resultDiv = document.querySelector(".result");
+
+//     // function booking() {
+//     //     resultDiv.textContent = firstName + " " + lastName
+
+//     // }
+
+//     // booking();
+
+//     const slider = document.getElementById("range");
+//     const output = document.getElementById("slideVal");
+//     output.textContent = slider.value;
+
+//     slider.oninput = function () {
+//         output.textContent = this.value;
+//     };
+
+
+
+
+//     // output.innerHTML = slider.value; // Display the default slider value
